f30a4c25a26cb42e1ca9cf1fb5b773e8a5c20e8f
kouyate karim <karimkouyate@adjemin.com>
2022-07-31 09:37:14 +0000

    test

---
+++ src/About.js
@@ -0,0 +1,9 @@
+function About(){
+    return (
+        <div>
+            Ceci est le composant About
+        </div>
+    );
+}
+
+export default About;
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import About from './About';
+// import './App.css';
+
+function App() {
+
+
+
+  return (
+    <>
+      <h1 className="title">Presentation React</h1>
+      <p>React est un frameworks fabuleux ! 😀</p>
+
+      <About/>
+    </>
+  );
+
+
+}
+
+export default App;
